09d0e326596a68d857b25105415dcf3cff084bbc
Henry <lihnry0813@gmail.com>
2025-03-09 18:35:29 +0000

    Update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,21 @@
 const siteMetadata = {
-  title: 'Blog | Parth Desai',
-  author: 'Parth Desai',
-  headerTitle: 'Mere Musings',
-  description: 'My personal blog where I share my musings',
-  snippets: 'Reuseable code snippets collected by Parth',
+  title: 'Blog | Henry Li',
+  author: 'Henry Li',
+  headerTitle: "Dragoon's Den",
+  description: 'My personal website and blog',
+  snippets: 'Reuseable code snippets collected by Henry',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://musing.vercel.app',
-  siteRepo: 'https://github.com/pycoder2000/blog',
+  siteUrl: 'https://personal-portfolio-website-yellowdragoons-projects.vercel.app/',
+  siteRepo: 'https://github.com/yellowdragoon/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'desaiparth2000@gmail.com',
-  github: 'https://github.com/pycoder2000',
-  twitter: 'https://twitter.com/_ParthDesai_',
-  linkedin: 'https://www.linkedin.com/in/parth-desai-2bb1b0160/',
-  website: 'https://parthdesai.vercel.app',
+  email: 'lih8@tcd.ie',
+  github: 'https://github.com/yellowdragoon',
+  twitter: 'https://x.com/yellowdragoon1',
+  linkedin: 'https://www.linkedin.com/in/henry-li-219a511b8/',
+  website: 'https://personal-portfolio-website-yellowdragoons-projects.vercel.app/',
   locale: 'en-US',
   analytics: {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
@@ -46,7 +46,7 @@ const siteMetadata = {
     },
   },
   socialAccount: {
-    twitter: '_ParthDesai_',
+    twitter: 'yellowdragoon1',
   },
 }
 
