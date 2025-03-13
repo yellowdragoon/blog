e6768308da3c5228752cf7c5d9e14fde51261759
Henry <lihnry0813@gmail.com>
2025-03-13 18:28:29 +0000

    Add google analytics id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,7 +20,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-F6V2QTJ628', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-HEVL095JVZ', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     provider: 'emailOctopus',
